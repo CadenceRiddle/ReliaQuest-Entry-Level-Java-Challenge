0882f5a99276d78d68f363eba7bc3743269550b2
CadenceRiddle <cadence475@gmail.com>
2025-02-07 15:53:27 -0500

    added attributes to employee implementation

--- api/src/main/java/com/challenge/api/model/EmployeeImp.java
+++ api/src/main/java/com/challenge/api/model/EmployeeImp.java
@@ -5,6 +5,17 @@ import java.time.Instant;
 
 
 
-public class EmployeeImp {
+public class EmployeeImp implements Employee{
+    private UUID uuid;
+    private String firstName;
+    private String lastName;
+    private String fullName;
+    private double salary;
+    private int age;
+    private String jobTitle;
+    private String email;
+    private Instant contractHireDate;
+    private Instant contractTerminationDate;
+
     
 }
