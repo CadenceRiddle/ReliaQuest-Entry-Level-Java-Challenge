d8488a7df5f7d7944119cafe0a3e0876864a9375
CadenceRiddle <cadence475@gmail.com>
2025-02-07 16:06:49 -0500

    finished class for employee implementation, added constructor and getters and setters

--- api/src/main/java/com/challenge/api/model/EmployeeImp.java
+++ api/src/main/java/com/challenge/api/model/EmployeeImp.java
@@ -1,21 +1,94 @@
 package com.challenge.api.model;
 
-import java.util.UUID;
+import java.util.UUID; //imported universally unique identifier and instant timestamps
 import java.time.Instant;
 
 
 
 public class EmployeeImp implements Employee{
-    private UUID uuid;
+    
+    private UUID uuid;//attributes for the employee
     private String firstName;
     private String lastName;
     private String fullName;
-    private double salary;
+    private int salary;
     private int age;
     private String jobTitle;
     private String email;
     private Instant contractHireDate;
     private Instant contractTerminationDate;
 
-    
+    //constructor for the employee
+    public EmployeeImp(UUID uuid, String firstName, String lastName, int salary, int age, String jobTitle, String email, Instant contractHireDate) {
+        this.uuid = uuid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.salary = salary;
+        this.age = age;
+        this.jobTitle = jobTitle;
+        this.email = email;
+        this.contractHireDate = contractHireDate;
+    }
+
+    //getters and setters for the employee
+    @override public UUID getUuid() {
+        return uuid;
+    }
+    @override public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+    @override public String getFirstName() {
+        return firstName;
+    }
+    @override public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    @override public String getLastName() {
+        return lastName;
+    }
+    @override public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+    @override public String getFullName() {
+        return fullName;
+    }
+    @override public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+    @override public int getSalary() {
+        return salary;
+    }
+    @override public void setSalary(int salary) {
+        this.salary = salary;
+    }
+    @override public int getAge() {
+        return age;
+    }
+    @override public void setAge(int age) {
+        this.age = age;
+    }
+    @override public String getJobTitle() {
+        return jobTitle;
+    }
+    @override public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+    @override public String getEmail() {
+        return email;
+    }
+    @override public void setEmail(String email) {
+        this.email = email;
+    }
+    @override public Instant getContractHireDate() {
+        return contractHireDate;
+    }
+    @override public void setContractHireDate(Instant contractHireDate) {
+        this.contractHireDate = contractHireDate;
+    }
+    @override public Instant getContractTerminationDate() {
+        return contractTerminationDate;
+    }
+    @override public void setContractTerminationDate(Instant contractTerminationDate) {
+        this.contractTerminationDate = contractTerminationDate;
+    }
 }
