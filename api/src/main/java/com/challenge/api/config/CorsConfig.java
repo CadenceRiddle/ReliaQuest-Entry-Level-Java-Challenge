b542044fb5a04f8710af473460aee92abc36bc98
CadenceRiddle <cadence475@gmail.com>
2025-02-08 01:28:39 -0500

    implemented Cors for unauthorized access prevention

---
+++ api/src/main/java/com/challenge/api/config/CorsConfig.java
@@ -0,0 +1,30 @@
+package com.challenge.api.config;
+
+/*
+ * Not required for submission, however helps with security for server resources
+ * Prevents unauthorized access and requests
+ */
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.List;
+
+
+public class CorsConfig {
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:3000")); //location of the frontend, adjust as needed
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));//submission only requires GET and POST
+        config.setAllowedHeaders(List.of("*"));
+        config.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
+}
--- api/src/main/java/com/challenge/api/controller/EmployeeController.java
+++ api/src/main/java/com/challenge/api/controller/EmployeeController.java
@@ -13,10 +13,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Fill in the missing aspects of this Spring Web REST Controller. Don't forget to add a Service layer.
  */
+@CrossOrigin(origins = "http://localhost:3000")//match with frontend in config directory
 @RestController
 @RequestMapping("/api/v1/employee")
 public class EmployeeController {
