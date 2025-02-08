1b4464479b20d9cefd29a51773fdd9eff4c93d3a
CadenceRiddle <cadence475@gmail.com>
2025-02-07 19:47:47 -0500

    created service file to handle business logic and mock database interaction

---
+++ api/src/main/java/com/challenge/api/service/EmployeeService.java
@@ -0,0 +1,11 @@
+package com.challenge.api.service;
+
+import com.challenge.api.model.Employee;//imports classes needed
+import com.challenge.api.model.EmployeeImpl;
+import org.springframework.stereotype.Service;
+import java.util.*;
+
+@Service
+public class EmployeeService {
+    
+}
