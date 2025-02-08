f76c1b8bc79a047240b819ef1ac35ee02517b826
CadenceRiddle <cadence475@gmail.com>
2025-02-07 20:36:21 -0500

    fixed minor issues with constructor and implementations

--- api/src/main/java/com/challenge/api/service/EmployeeService.java
+++ api/src/main/java/com/challenge/api/service/EmployeeService.java
@@ -1,19 +1,23 @@
 package com.challenge.api.service;
 
 import com.challenge.api.model.Employee;//imports classes needed
-import com.challenge.api.model.EmployeeImpl;
+import com.challenge.api.model.EmployeeImp;
 import org.springframework.stereotype.Service;
 import java.util.*;
+import java.time.Instant;
 
 @Service
 public class EmployeeService {
     private final Map<UUID, Employee> mockDatabase = new HashMap<>(); //creates a map to store employees
+    
+    //Note: the below constructor needs to be commented if you want to run the tests
 
     public EmployeeService() {//constructor to add mock employees
-        Employee emp1 = new EmployeeImpl(UUID.randomUUID(), "Alice", "Johnson", "Software Engineer", "alice@example.com", 90000, 28);
-        Employee emp2 = new EmployeeImpl(UUID.randomUUID(), "Bob", "Smith", "Product Manager", "bob@example.com", 120000, 35);
-        mockDatabase.put(emp1.getUuid(), emp1);
-        mockDatabase.put(emp2.getUuid(), emp2);
+        //Employee emp1 = new EmployeeImp(UUID.randomUUID(), "Alice", "Johnson", 90000, 28, "Software Engineer", "alice@example.com", Instant.now());
+        //Employee emp2 = new EmployeeImp(UUID.randomUUID(), "John", "Doe", 80000, 30, "Software Engineer", "john.doe@example.com", Instant.now());
+
+        //mockDatabase.put(emp1.getUuid(), emp1);
+        //mockDatabase.put(emp2.getUuid(), emp2);
     }
 
     public List<Employee> getAllEmployees() {   //method to retrieve all employees
